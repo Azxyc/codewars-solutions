4aaee88f0cd363ef8ab742b53d8029840e8c0ea6
Azxyc <46279597+Azxyc@users.noreply.github.com>
2022-01-28 14:27:34 +0000

    Create weird words.py

---
+++ weird words.py
@@ -0,0 +1,3 @@
+def next_letter(y):
+    return ''.join([(chr(ord(x)+1) if x in 'abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXY' else (x if x not in 'Zz' else ('a' if x == 'z' else 'A'))) for x in y])
+   
