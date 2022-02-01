dd0c5d18da4d8b548f77e59644f8909e90031e37
Azxyc <46279597+Azxyc@users.noreply.github.com>
2022-02-01 14:47:33 +0000

    Readability

--- weird words.py
+++ weird words.py
@@ -1,3 +1,7 @@
 def next_letter(y):
-    return ''.join([(chr(ord(x)+1) if x in 'abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXY' else (x if x not in 'Zz' else ('a' if x == 'z' else 'A'))) for x in y])
+    return ''.join([(chr(ord(x)+1)
+                     if x in 'abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXY'
+                     else (x if x not in 'Zz'
+                           else ('a' if x == 'z' 
+                                 else 'A'))) for x in y])
    
